c7b15c7292a0c968ee806429bd07dbcec91c0878
golubovsky <golubovsky@gmail.com>
2021-07-26 00:14:32 -0400

    First somehow working version of the MIDI Repeater
    Signed-off-by: golubovsky <golubovsky@gmail.com>

--- midirep.lua
+++ midirep.lua
@@ -4,23 +4,82 @@ ardour {
  category    = "Utility",
  license     = "MIT",
  author      = "Dmitry Golubovsky",
- description = [[Midi Repeater - based on midi filter example.]]
+ description = [[Midi Repeater - based on midi filter example - v1.]]
 }
 
 function dsp_ioconfig ()
  return { { midi_in = 1, midi_out = 1, audio_in = 0, audio_out = 0}, }
 end
 
+function dsp_params ()
+ return
+ {
+  { ["type"] = "input", name = "Pass Thru", min = 0, max = 1, default = 0, toggled = true },
+  { ["type"] = "input", name = "Buffer Length", min = 3, max = 31, default = 7, integer = true } ,
+  { ["type"] = "input", name = "Beats/Bar", min = 1, max = 32, default = 4, integer = true },
+ }
+end
+
+local rate = 0
+
+function dsp_init (dsprate)
+  rate = dsprate
+end
+
+local notebuf = {}
+
+
+local mididx = 0
+local midrdx = 3
+
+local time = 0
+local tme = 0
+
 function dsp_run (_, _, n_samples)
  assert (type(midiin) == "table")
  assert (type(midiout) == "table")
- local cnt = 1;
+ local ctrl = CtrlPorts:array()
+ local pt = ctrl[1]
+ local buflen = ctrl[2]
+ local beats = ctrl[3]
+ local cnt = 1
+ local tstop = Session:transport_stopped ()
+ local tm = Session:tempo_map ()
+ local ts = tm:tempo_section_at_sample (0)
+ local bpm = ts:to_tempo():note_types_per_minute ()
+ local nt = ts:to_tempo():note_type ()
 
- function tx_midi (time, data)
+ function tx_midi (time_, data_)
   midiout[cnt] = {}
-  midiout[cnt]["time"] = time;
-  midiout[cnt]["data"] = data;
+  midiout[cnt]["time"] = time_;
+  midiout[cnt]["data"] = data_;
   cnt = cnt + 1;
+ end
+
+ -- replay buffer if transport is running
+ 
+ if not tstop then 
+   for time = 1, n_samples do
+     tme = tme + 1;
+     if tme >= rate * 60 * nt / (bpm * beats) then
+	   local ridx = midrdx%buflen
+	   local pn = notebuf [ridx]
+	   if pn ~= nil and #pn == 3 then
+	           local chan = pn [1] & 15
+                   pn[1] = (8 << 4) | chan
+		   tx_midi(1, pn)
+	   end
+	   midrdx = midrdx + 1;
+	   ridx = midrdx%buflen
+	   pn = notebuf [ridx]
+	   if pn ~= nil and #pn == 3 then
+	           local chan = pn [1] & 15
+                   pn[1] = (9 << 4) | chan
+		   tx_midi(2, pn)
+	   end
+  	   tme = 0
+     end
+   end
  end
 
  -- for each incoming midi event
@@ -31,9 +90,12 @@ function dsp_run (_, _, n_samples)
   if #d == 0 then event_type = -1 else event_type = d[1] >> 4 end
 
   if (#d == 3 and event_type == 9) then -- note on
-   tx_midi (t, d)
+  if (pt == 1) then tx_midi (t, d) end
+   d[3] = 50
+   notebuf[mididx%buflen] = d
+   mididx = mididx + 1
   elseif (#d == 3 and event_type == 8) then -- note off
-   tx_midi (t, d)
+  if (pt == 1) then  tx_midi (t, d) end
   end
  end
 end
